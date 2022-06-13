b99477b13fba2988a36e94e4d5e7919c716880aa
marcelovfernandes <106869914+marcelovfernandes@users.noreply.github.com>
2022-06-13 17:24:42 -0300

    17 commit

--- scripts/index-p14-scroller-sem-looping.js
+++ scripts/index-p14-scroller-sem-looping.js
@@ -1,58 +1,36 @@
 // Fiz este teste eliminando os loopings de criação do cabeçalho dinâmico, para ter maior controle sobre a visibilidade das colunas conforme diferentes opções de busca (ver infos/mapeamento.txt)
 
+// Tabela padrão: 
+
 $( document ).ready(function() { 
-    geraTable()
+    geraTable1()
 })
 
-// Início da função geraTable:
 
-function geraTable () {
+// Botões de escolha:
+
+const link1 = document.getElementById('link1');
+const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+const link4 = document.getElementById('link4');
+
+link1.onclick = (e) => {geraTable1()};
+link2.onclick = (e) => {geraTable2()};
+link3.onclick = (e) => {geraTable3()};
+link4.onclick = (e) => {geraTable4()};
+
+
+// Tabelas geradas a partir da escolha:
+
+// Função geraTable1 (default: buscando pelo português) (ver infos/mapeamento.txt, Opção 1):
+
+  function geraTable1 () {
 
         var data = dataSet1;
         var mydata = data.slice(1, data.lenght);
 
-		    // for (var i = 1; i < data[0].length; i++) {
-        //   document.getElementById("cabecalhoDinamico").innerHTML =
-        //   document.getElementById("cabecalhoDinamico").innerHTML +
-        //   `<th style="display: none">` +
-        //   data[0][i] +
-        //   `</th>`;
-        //   }
-	  
-        // data = data.slice(1, data.length);
-	      // var qualTd = data[0].length;
-
-        // var stringParaStyle = ``;
-        // var stringParaSearch = [];
-
-        // // console.log(qualTd);
-        // for (var i = 2; i < qualTd + 1; i++) {
-	      //   stringParaSearch.push(i - 1);
-
-        //   // console.log(i);
-    
-        //   stringParaStyle =
-	      //   stringParaStyle +
-        //     `#example .even > td:nth-child(` +
-        //     i.toString() +
-        //     `) {
-        //     display: none
-        //       }
-        //       #example .odd > td:nth-child(` +
-        //     i.toString() +
-        //     `) {
-        //     display: none
-        //       }`;
-        //     }
-
-        //   // console.log(stringParaSearch);
-
-        // var style = document.createElement("style");
-        // style.innerHTML = stringParaStyle;
-
-        // document.getElementsByTagName("body")[0].appendChild(style);
-
         $("#example").DataTable({
+            destroy: true,
             dom: '<f>t',
             data: mydata,
             mark: true,
@@ -63,17 +41,14 @@ function geraTable () {
             paginate: true,
             scrollY: 400,
             info: false,
-		        // columnDefs: [
-			      //   {targets: stringParaSearch, searchable: false }
-				    //   ],
             columnDefs: [{
-                targets: [1, 2, 3, 4], //Opção 1 - default: ver infos/mapeamento.txt
+                targets: [1, 2, 3, 4],
                 visible: false,
                 searchable: false,
             }],
             language: {
                 search: 'Buscar',
-                zeroRecords: 'Sem resultados para essa busca',
+                zeroRecords: 'Sem resultados para essa busca.',
             }
           });
           $('#example tbody').on('click', 'tr',function(){
@@ -86,22 +61,131 @@ function geraTable () {
             '<div>' + data[3] + '</div>' + 
             '<div>Fonte: ' + data[4] + '</div>';
           });
-      }; //Opção 1 - default: ver infos/mapeamento.txt
-
-// Fim da função geraTable.
+      };
 
 
+// Função geraTable2 (buscando pelo inglês) (ver infos/mapeamento.txt, Opção 2):
+
+  function geraTable2 () {
+
+        var data = dataSet1;
+        var mydata = data.slice(1, data.lenght);
+
+        $("#example").DataTable({
+            destroy: true,
+            dom: '<f>t',
+            data: mydata,
+            mark: true,
+            ordering: false,
+            deferRender: true,
+            scrollCollapse: false,
+            scroller: true,
+            paginate: true,
+            scrollY: 400,
+            info: false,
+            columnDefs: [{
+                targets: [0, 1, 3, 4], 
+                visible: false,
+                searchable: false,
+            }],
+            language: {
+                search: 'Search',
+                zeroRecords: 'No matching records found.',
+            }
+          });
+          $('#example tbody').on('click', 'tr',function(){
+            let table = $('#example').DataTable();
+            var data = table.row(this).data();
+            document.getElementById('corpo').innerHTML = 
+            '<div>' + data[2] + '</div>' + 
+            '<div>' + data[1] + '</div>' + 
+            '<div>' + data[0] + '</div>' + 
+            '<div>' + data[3] + '</div>' + 
+            '<div>Fonte: ' + data[4] + '</div>';
+          });
+      }; 
 
 
-  // Nestas últimas linhas, usei a api row().data() do dataTables (https://datatables.net/reference/api/row().data())
+// Função geraTable3 (buscando pelo francês) (ver infos/mapeamento.txt, Opção 3):
+
+  function geraTable3 () {
+
+        var data = dataSet1;
+        var mydata = data.slice(1, data.lenght);
+
+        $("#example").DataTable({
+            destroy: true,
+            dom: '<f>t',
+            data: mydata,
+            mark: true,
+            ordering: false,
+            deferRender: true,
+            scrollCollapse: false,
+            scroller: true,
+            paginate: true,
+            scrollY: 400,
+            info: false,
+            columnDefs: [{
+                targets: [0, 1, 2, 4],
+                visible: false,
+                searchable: false,
+            }],
+            language: {
+                search: 'Chercher',
+                zeroRecords: 'Pas de résultat pour cette recherche.',
+            }
+          });
+          $('#example tbody').on('click', 'tr',function(){
+            let table = $('#example').DataTable();
+            var data = table.row(this).data();
+            document.getElementById('corpo').innerHTML = 
+            '<div>' + data[3] + '</div>' + 
+            '<div>' + data[1] + '</div>' + 
+            '<div>' + data[0] + '</div>' + 
+            '<div>' + data[2] + '</div>' + 
+            '<div>Fonte: ' + data[4] + '</div>';
+          });
+      }; 
+
+// Função geraTable4 (buscando pelo latim) (ver infos/mapeamento.txt, Opção 4):
+
+  function geraTable4 () {
+
+        var data = dataSet1;
+        var mydata = data.slice(1, data.lenght);
+
+        $("#example").DataTable({
+            destroy: true,
+            dom: '<f>t',
+            data: mydata,
+            mark: true,
+            ordering: false,
+            deferRender: true,
+            scrollCollapse: false,
+            scroller: true,
+            paginate: true,
+            scrollY: 400,
+            info: false,
+            columnDefs: [{
+                targets: [0, 2, 3, 4], 
+                visible: false,
+                searchable: false,
+            }],
+            language: {
+                search: 'Petere',
+                zeroRecords: 'Petīta inventa non sunt.',
+            }
+          });
+          $('#example tbody').on('click', 'tr',function(){
+            let table = $('#example').DataTable();
+            var data = table.row(this).data();
+            document.getElementById('corpo').innerHTML = 
+            '<div>' + data[1] + '</div>' + 
+            '<div>' + data[0] + '</div>' + 
+            '<div>' + data[2] + '</div>' + 
+            '<div>' + data[3] + '</div>' + 
+            '<div>Fonte: ' + data[4] + '</div>';
+          });
+      }; 
+
   
-  //O código original (para construção dinâmica dos resultados a partir da tabela) era este:
-
-    // $(document.getElementById("example")).on("click", "tr", function (e) {
-    //   document.getElementById("corpo").innerHTML = "";
-    //   for (var i = 1; i < data[0].length; i++) {
-    //     document.getElementById("corpo").innerHTML =
-    //       document.getElementById("corpo").innerHTML + "<div>" +
-    //       e.currentTarget.getElementsByTagName("td")[i].innerHTML + "</div>";
-    //   }
-    // });
