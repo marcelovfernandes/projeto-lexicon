d11f2458982dd0490c8c3ee76672e2580df6b701
marcelovfernandes <106869914+marcelovfernandes@users.noreply.github.com>
2022-06-13 09:44:26 -0300

    14 commit

---
+++ scripts/index-p14-scroller-sem-looping.js
@@ -0,0 +1,100 @@
+// Fiz este teste eliminando os loopings de criação do cabeçalho dinâmico.
+
+$( document ).ready(function() { 
+    geraTable()
+})
+
+// Início da função geraTable:
+
+function geraTable () {
+
+        var data = dataSet1;
+        var mydata = data.slice(1, data.lenght);
+
+		    // for (var i = 1; i < data[0].length; i++) {
+        //   document.getElementById("cabecalhoDinamico").innerHTML =
+        //   document.getElementById("cabecalhoDinamico").innerHTML +
+        //   `<th style="display: none">` +
+        //   data[0][i] +
+        //   `</th>`;
+        //   }
+	  
+        // data = data.slice(1, data.length);
+	      // var qualTd = data[0].length;
+
+        // var stringParaStyle = ``;
+        // var stringParaSearch = [];
+
+        // // console.log(qualTd);
+        // for (var i = 2; i < qualTd + 1; i++) {
+	      //   stringParaSearch.push(i - 1);
+
+        //   // console.log(i);
+    
+        //   stringParaStyle =
+	      //   stringParaStyle +
+        //     `#example .even > td:nth-child(` +
+        //     i.toString() +
+        //     `) {
+        //     display: none
+        //       }
+        //       #example .odd > td:nth-child(` +
+        //     i.toString() +
+        //     `) {
+        //     display: none
+        //       }`;
+        //     }
+
+        //   // console.log(stringParaSearch);
+
+        // var style = document.createElement("style");
+        // style.innerHTML = stringParaStyle;
+
+        // document.getElementsByTagName("body")[0].appendChild(style);
+
+        $("#example").DataTable({
+            dom: '<f>t',
+            data: mydata,
+            mark: true,
+            ordering: false,
+            deferRender: true,
+            scrollCollapse: false,
+            scroller: true,
+            paginate: true,
+            scrollY: 400,
+            info: false,
+            columns: [0],
+		        // columnDefs: [
+			      //   {targets: stringParaSearch, searchable: false }
+				    //   ],
+            language: {
+                search: 'Buscar',
+                zeroRecords: 'Sem resultados para essa busca',
+            }
+          });
+          $('#example tbody').on('click', 'tr',function(){
+            let table = $('#example').DataTable();
+            var data = table.row(this).data();
+            document.getElementById('corpo').innerHTML = 
+            '<div>' + data[0] + '</div>' + 
+            '<div>' + data[1] + '</div>' + 
+            '<div>' + data[2] + '</div>' + 
+            '<div>' + data[3] + '</div>' + 
+            '<div>Fonte: ' + data[4] + '</div>';
+          });
+      };
+
+// Fim da função geraTable.
+
+  // Nestas últimas linhas, usei a api row().data() do dataTables (https://datatables.net/reference/api/row().data())
+  
+  //O código original (para construção dinâmica dos resultados a partir da tabela) era este:
+
+    // $(document.getElementById("example")).on("click", "tr", function (e) {
+    //   document.getElementById("corpo").innerHTML = "";
+    //   for (var i = 1; i < data[0].length; i++) {
+    //     document.getElementById("corpo").innerHTML =
+    //       document.getElementById("corpo").innerHTML + "<div>" +
+    //       e.currentTarget.getElementsByTagName("td")[i].innerHTML + "</div>";
+    //   }
+    // });
